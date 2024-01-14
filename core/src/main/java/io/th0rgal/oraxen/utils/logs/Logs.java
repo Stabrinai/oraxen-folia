b6a3d8356cbb4233bfd96e1a0bc02dcce702d85a
Bierque Jason <bierquejason@gmail.com>
2024-01-14 14:06:12 +0100

    Oups

--- core/src/main/java/io/th0rgal/oraxen/utils/logs/Logs.java
+++ core/src/main/java/io/th0rgal/oraxen/utils/logs/Logs.java
@@ -14,53 +14,44 @@ public class Logs {
     private Logs() {}
 
     public static void logInfo(String message) {
-        System.out.println("loginfo1");
         logInfo(message, false);
     }
 
     private static final TagResolver PrefixResolver = TagResolver.resolver("prefix", Tag.selfClosingInserting(AdventureUtils.MINI_MESSAGE_EMPTY.deserialize(Message.PREFIX.toString())));
 
     public static void logInfo(String message, boolean newline) {
-        System.out.println("loginfo2");
         Component info = AdventureUtils.MINI_MESSAGE_EMPTY.deserialize("<prefix><#529ced>" + message + "</#529ced>", PrefixResolver);
         OraxenPlugin.get().getAudience().console().sendMessage(newline ? info.append(Component.newline()) : info);
     }
 
     public static void logSuccess(String message) {
-        System.out.println("loginfo3");
         logSuccess(message, false);
     }
 
     public static void logSuccess(String message, boolean newline) {
-        System.out.println("loginfo4");
         Component success = AdventureUtils.MINI_MESSAGE_EMPTY.deserialize("<prefix><#55ffa4>" + message + "</#55ffa4>", PrefixResolver);
         OraxenPlugin.get().getAudience().console().sendMessage(newline ? success.append(Component.newline()) : success);
     }
 
     public static void logError(String message) {
-        System.out.println("loginfo5");
         logError(message, false);
     }
 
     public static void logError(String message, boolean newline) {
-        System.out.println("loginfo6");
         Component error = AdventureUtils.MINI_MESSAGE_EMPTY.deserialize("<prefix><#e73f34>" + message + "</#e73f34>", PrefixResolver);
         OraxenPlugin.get().getAudience().console().sendMessage(newline ? error.append(Component.newline()) : error);
     }
 
     public static void logWarning(String message) {
-        System.out.println("loginfo7");
         logWarning(message, false);
     }
 
     public static void logWarning(String message, boolean newline) {
-        System.out.println("loginfo8");
         Component warning = AdventureUtils.MINI_MESSAGE_EMPTY.deserialize("<prefix><#f9f178>" + message + "</#f9f178>", PrefixResolver);
         OraxenPlugin.get().getAudience().console().sendMessage(newline ? warning.append(Component.newline()) : warning);
     }
 
     public static void newline() {
-        System.out.println("loginfo9");
         OraxenPlugin.get().getAudience().console().sendMessage(Component.empty());
     }
 
